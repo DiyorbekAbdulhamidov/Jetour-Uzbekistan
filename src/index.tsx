f034fe43e2b099155eaaa481bab5fbbb90b7acc4
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-06-27 13:08:11 +0500

    fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import "../src/components/fonts/arimo.css";
+import "../src/components/fonts/open-sans.css";
+import "../src/components/fonts/poppins.css";
 import './index.css';
 import App from './app';
 import About from './components/about/about';
@@ -10,7 +12,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} /> 
+      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')

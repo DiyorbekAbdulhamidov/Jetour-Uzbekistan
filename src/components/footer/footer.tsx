9e183bbd183170354286566a92b63fd840f681f8
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-07-12 15:19:02 +0500

    codes

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -30,8 +30,8 @@ function Footer() {
     try {
       await axios.post(API_URL, data).then((response) => {
         setPhoneNumber("");
-        // setShowAlert(true);
-        toast.success("Ваше сообщение успешно отправлено..");
+        setShowAlert(true);
+        // toast.success("Ваше сообщение успешно отправлено..");
 
       })
     }
